c960504373ad589ae6ded270860901217e6d2670
Ryea Mahansaria <ryeamahansaria@gmail.com>
2023-07-20 13:56:34 -0500

    New calendar commit

---
+++ Study Buddy/calender/DateHolder.swift
@@ -0,0 +1,14 @@
+//
+//  DateHolder.swift
+//  calender
+//
+//  Created by Ryea Mahansaria on 7/19/23.
+//
+
+import Foundation
+
+
+class DateHolder:ObservableObject
+{
+    @Published var date = Date() //todays date
+}
